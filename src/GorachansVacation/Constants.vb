9142ff79adba13dd8887ed49f2e820ff54356252
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-13 05:16:20 -0500

    title screen is amazing

---
+++ src/GorachansVacation/Constants.vb
@@ -0,0 +1,4 @@
+﻿Friend Module Constants
+    Friend Const GameTitle = "Gorachan's Vacation IV: Just the Tip"
+    Friend Const QuitText = "Quit"
+End Module
--- src/GorachansVacation/Program.vb
+++ src/GorachansVacation/Program.vb
@@ -2,9 +2,12 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        Console.Title = GameTitle
         AnsiConsole.Clear()
-        Dim prompt = New SelectionPrompt(Of String) With {.Title = "[aqua]Gorachan's Vacation IV: Just the Tip[/]"}
-        prompt.AddChoice("Quit")
+        Dim figlet As New FigletText(GameTitle) With {.Color = Color.Red, .Justification = Justify.Center}
+        AnsiConsole.Write(figlet)
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = ""}
+        prompt.AddChoice(QuitText)
         AnsiConsole.Prompt(prompt)
     End Sub
 End Module
