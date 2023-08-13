cdd44cecdc62827353b5818bf5e061d5b71b3a2c
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-08-13 16:27:46 -0500

    add persistence tests

---
+++ src/GV.Persistence.Tests/Message_should.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Security.Cryptography.X509Certificates
+
+Public Class Message_should
+    Private Function CreateSubject() As IMessage
+        Dim world As IWorld = New World(New Data.WorldData)
+        Return world.CreateMessage
+    End Function
+    <Fact>
+    Sub hold_statistics()
+        Dim subject As IMessage = CreateSubject()
+        DoStatisticHolderTests(subject)
+    End Sub
+    <Fact>
+    Sub hold_metadata()
+        Dim subject As IMessage = CreateSubject()
+        DoMetadataHolderTests(subject)
+    End Sub
+    <Fact>
+    Sub hold_flags()
+        Dim subject As IMessage = CreateSubject()
+        DoFlagHolderTests(subject)
+    End Sub
+    <Fact>
+    Sub has_properties()
+        Dim subject As IMessage = CreateSubject()
+        subject.LineCount.ShouldBe(0)
+        subject.Lines.ShouldBeEmpty
+        subject.ChoiceCount.ShouldBe(0)
+        subject.Choices.ShouldBeEmpty
+        subject.HasChoices.ShouldBeFalse
+        subject.Sfx.ShouldBeNull
+    End Sub
+    <Fact>
+    Sub read_and_write_sfx()
+        Const Sfx = "Sfx"
+        Dim subject As IMessage = CreateSubject()
+        subject.Sfx = Sfx
+        subject.Sfx.ShouldBe(Sfx)
+    End Sub
+    <Fact>
+    Sub add_choice()
+        Const Text = "Text"
+        Const EffectType = "EffectType"
+        Dim subject As IMessage = CreateSubject()
+        subject.AddChoice(Text, EffectType)
+        subject.HasChoices.ShouldBeTrue
+        subject.Choices.ShouldNotBeEmpty
+        subject.ChoiceCount.ShouldBe(1)
+        subject.Choice(0).Text.ShouldBe(Text)
+        subject.Choice(0).EffectType.ShouldBe(EffectType)
+    End Sub
+    <Fact>
+    Sub add_line()
+        Const Text = "Text"
+        Const Hue = 1
+        Dim subject As IMessage = CreateSubject()
+        subject.AddLine(Hue, Text)
+        subject.Lines.ShouldNotBeEmpty
+        subject.LineCount.ShouldBe(1)
+    End Sub
+End Class
---
+++ src/GV.Persistence.Tests/World_should.vb
@@ -0,0 +1,89 @@
+Public Class World_should
+    <Fact>
+    Sub hold_statistics()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        DoStatisticHolderTests(subject)
+    End Sub
+
+    <Fact>
+    Sub hold_metadata()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        DoMetadataHolderTests(subject)
+    End Sub
+
+    <Fact>
+    Sub hold_flags()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        DoFlagHolderTests(subject)
+    End Sub
+
+    <Fact>
+    Sub create_map()
+        Const mapType = "MapType"
+        Const columns = 3
+        Const rows = 2
+        Const terrainType = "TerrainType"
+        Dim subject As IWorld = New World(New Data.WorldData)
+        Dim actual = subject.CreateMap(mapType, (columns, rows), terrainType)
+        actual.ShouldNotBeNull
+        actual.Columns.ShouldBe(columns)
+        actual.Rows.ShouldBe(rows)
+        actual.Cells.Count.ShouldBe(columns * rows)
+        actual.Cells.ShouldAllBe(Function(x) x.TerrainType = terrainType)
+        subject.Maps.ShouldHaveSingleItem
+        subject.Map(0).Id.ShouldBe(actual.Id)
+    End Sub
+    <Fact>
+    Sub create_character()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        Dim map = subject.CreateMap("MapType", (3, 2), "TerrainType")
+        Dim actual = subject.CreateCharacter("CharacterType", map.GetCell(2, 1))
+        actual.ShouldNotBeNull
+        actual.Cell.Column.ShouldBe(2)
+        actual.Cell.Row.ShouldBe(1)
+        subject.Characters.ShouldHaveSingleItem
+        subject.Character(0).Id.ShouldBe(actual.Id)
+    End Sub
+    <Fact>
+    Sub begin_blank()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        subject.Avatar.ShouldBeNull
+        subject.Maps.ShouldBeEmpty
+        subject.Characters.ShouldBeEmpty
+        subject.CurrentMessage.ShouldBeNull
+        subject.HasMessages.ShouldBeFalse
+    End Sub
+    <Fact>
+    Sub create_message()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        Dim actual = subject.CreateMessage
+        actual.ShouldNotBeNull
+        subject.CurrentMessage.ShouldNotBeNull
+        subject.HasMessages.ShouldBeTrue
+    End Sub
+    <Fact>
+    Sub create_item()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        Dim actual = subject.CreateItem("ItemType")
+        actual.ShouldNotBeNull
+        subject.Item(0).Id.ShouldBe(actual.Id)
+    End Sub
+    <Fact>
+    Sub dismiss_message()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        subject.CreateMessage()
+        subject.CurrentMessage.ShouldNotBeNull
+        subject.HasMessages.ShouldBeTrue
+        subject.DismissMessage()
+        subject.CurrentMessage.ShouldBeNull
+        subject.HasMessages.ShouldBeFalse
+    End Sub
+    <Fact>
+    Sub serialize_data()
+        Dim subject As IWorld = New World(New Data.WorldData)
+        Dim actual = subject.SerializedData
+        actual.ShouldBe("{""Maps"":[],""Characters"":[],""AvatarCharacterId"":null,""Messages"":[],""Items"":[],""Statistics"":{},""Flags"":[],""Metadatas"":{}}")
+    End Sub
+End Class
+
+
